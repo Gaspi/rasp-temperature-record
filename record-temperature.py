ba1a08e9b8223672cad4744cbcd192f857cb7766
Gaspard Férey <gaspard.ferey@hotmail.fr>
2023-08-07 23:46:01 +0200

    WIP

--- record-temperature.py
+++ record-temperature.py
@@ -1,4 +1,12 @@
+from sys import argv
+from datetime import datetime
+import Adafruit_DHT
 
-
-
-
+if __name__ == "__main__":
+    if len(argv) == 0:
+        print("Missing argument")
+        exit 1
+    with open(argv[0], "a") as outfile:
+        humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+        now = datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
+        outfile.write("{},{},{},{}\n".format(now, temperature, humidity) )
