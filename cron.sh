7211951c4018724c7c243fc50421672ca66adb1b
Gaspard FEREY <gaspard.ferey@hotmail.fr>
2023-08-08 10:49:16 +0200

    WIP

--- cron.sh
+++ cron.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Raspberry temperature recorder
-python3 "[INSTALL_FOLDER]/record-temperature.py" "[INSTALL_FOLDER]/data"
+python3 "[INSTALL_FOLDER]/record-temperature.py" "[INSTALL_FOLDER]/data" > "[LOG_FOLDER]/temprecord.log"
--- record-temperature.py
+++ record-temperature.py
@@ -4,9 +4,9 @@ from datetime import datetime
 import Adafruit_DHT
 
 if __name__ == "__main__":
-    if len(argv) == 0:
-        print("Missing argument")
-        exit 1
+    if len(argv) < 2:
+        print("Missing output file argument")
+        exit(1)
     data = []
     start = datetime.now()
     data.append( start.strftime("%Y-%m-%d") )
@@ -15,9 +15,7 @@ if __name__ == "__main__":
         humidity, temperature = Adafruit_DHT.read_retry(11, 4)
         data.append(str(humidity))
         data.append(str(temperature))
-        sleep(10)
-    end = datetime.now()
-    data.append( end.strftime("%Y-%m-%d") )
-    data.append( end.strftime("%H:%M:%S") )
-    with open(argv[0], "a") as outfile:
+        sleep(5)
+    data.append( datetime.now().strftime("%H:%M:%S") )
+    with open(argv[1], "a") as outfile:
         outfile.write(",".join(data)+"\n")
