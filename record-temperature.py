9b07d5d6102a99a3f203257c29855c194c02210b
Gaspard Férey <gaspard.ferey@hotmail.fr>
2023-09-01 08:09:46 +0200

    Update record-temperature.py

--- record-temperature.py
+++ record-temperature.py
@@ -7,15 +7,10 @@ if __name__ == "__main__":
     if len(argv) < 2:
         print("Missing output file argument")
         exit(1)
-    data = []
-    start = datetime.now()
-    data.append( start.strftime("%Y-%m-%d") )
-    data.append( start.strftime("%H:%M:%S") )
-    for i in range(6):
-        humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-        data.append(str(humidity))
-        data.append(str(temperature))
-        sleep(5)
-    data.append( datetime.now().strftime("%H:%M:%S") )
-    with open(argv[1], "a") as outfile:
-        outfile.write(",".join(data)+"\n")
+    Adafruit_DHT.read_retry(11, 4)
+    sleep(5)
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    now = int( datetime.now().timestamp() )
+        if humidity is not None and temperature is not None:
+            with open(argv[1], "a") as outfile:
+                outfile.write( "{},{},{}\n".format(now, humidity, temperature) )
